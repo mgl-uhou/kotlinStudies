06d0a90afeaa28483a95c78ecc493b17ed161b7c
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-06-19 15:34:44 -0300

    updating

--- src/main/kotlin/oop/Car.kt
+++ src/main/kotlin/oop/Car.kt
@@ -1,8 +1,11 @@
 package oop
 
+//import kotlin.reflect.full.memberProperties
+
 class Car {
+    var name: String = "Car"
     var motor: Double = 2.0
-    val colour: String = "Prata"
+    val colour: String = "Silver"
 
     fun start(): Unit {
         println("Rundundundundundundundundundun")
@@ -10,10 +13,23 @@ class Car {
 }
 
 fun main(){
-    var mustang: Car = Car()
+    var car1: Car = Car()
 
-    mustang.motor = 2.4
-    println(mustang.colour)
-    println(mustang.motor)
-    mustang.start()
+    car1.name = "Mustang"
+    car1.motor = 2.4
+    println(car1.colour)
+    println(car1.motor)
+    car1.start()
+
+    println(car1)
 }
+
+/*
+fun printProperties(obj: Any){
+    val kClass = obj::class
+    println("Properties of ${kClass.simpleName}")
+    for (prop in kClass.memberProperties) {
+        val value = prop.getter.call(obj)
+        println("${prop.name} = $value")
+    }
+}*/
