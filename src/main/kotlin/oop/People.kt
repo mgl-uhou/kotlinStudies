1d8ecb039880f205527466438f29ccbb515c9b5d
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-07-16 16:48:13 -0300

    Updating

--- src/main/kotlin/oop/People.kt
+++ src/main/kotlin/oop/People.kt
@@ -5,6 +5,22 @@ class People {
     constructor(firstName: String){ // Secondary Constructor
         this.firstName = firstName
     }
+
+    override fun equals(other: Any?): Boolean = when{ // == automatically calls this function
+        /*if (this === other) return true
+        if (other !is People) return false
+        return this.firstName == other.firstName*/
+
+        // Example with no when above
+
+        this === other -> true
+        other !is People -> false
+        else -> this.firstName == other.firstName
+    }
+
+    override fun hashCode(): Int {
+        return this.firstName.hashCode() * 31
+    }
 }
 
 class People2(var firstName: String) {
@@ -23,8 +39,10 @@ fun main(){
     person2.lastName = "Laurentino"
     println(person2.firstName + " " + person2.lastName)
 
-    val p = People("Mikaelly")
+    val p = People("Miguel")
     println(person)
     println(p)
-    println(person === p)
+    println(person == p)
+    println(person.hashCode())
+    println(person.firstName.hashCode() == p.firstName.hashCode())
 }
