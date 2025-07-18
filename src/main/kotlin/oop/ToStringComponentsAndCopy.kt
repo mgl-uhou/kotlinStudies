82a4dd2035b64360852e58a10876f766c2332aee
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-07-18 10:51:51 -0300

    using Components for destructing and Randon function

--- src/main/kotlin/oop/ToStringComponentsAndCopy.kt
+++ src/main/kotlin/oop/ToStringComponentsAndCopy.kt
@@ -1,10 +1,13 @@
 package oop
 
-class Piece(var id: Long, var name: String){
+import kotlin.random.Random
+import kotlin.random.nextLong
+
+class Product(var id: Long, var name: String){
 
     override fun equals(other: Any?): Boolean = when {
         this === other        -> true
-        other !is Piece       -> false
+        other !is Product       -> false
         else                  -> this.id == other.id && this.name == other.name
     }
 
@@ -13,14 +16,23 @@ class Piece(var id: Long, var name: String){
     }
 
     override fun toString(): String {
-        // When printing an object of Piece class (println(object))
+        // When printing an object of product class (println(object))
         // It will be printed ⬇️
         return "ID: ${this.id} \nName: ${this.name}"
     }
+
+    operator fun component1() = this.id
+    operator fun component2() = this.name
 }
 
 fun main(){
 
-    val piece1 = Piece(923456789423, "Cama Box Casal")
-    println(piece1)
+    val product1 = Product(randomId(), "Cama King Casal")
+    println(product1)
+
+    val (id, name) = Product(id = randomId(), name = "Travesseiro")
+
+    println("$id, $name")
 }
+
+fun randomId(): Long = Random.nextLong(100000000000, 999999999999)
